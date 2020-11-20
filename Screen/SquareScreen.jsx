bcb0131d6876f406e55a03134fa01ed393fdfa0c
WahajAhmedIqbal <wahaj465@gmail.com>
2020-11-20 12:08:43 +0500

    reducer

--- Screen/SquareScreen.jsx
+++ Screen/SquareScreen.jsx
@@ -1,28 +1,49 @@
 import React, { useState } from "react";
 import { View } from "react-native";
 import ColorCounter from "./ColorCounter";
+
+const COLOR_COUNTER = 15;
+
 const SquareScreen = () => {
   const [red, setRed] = useState(0);
   const [green, setGreen] = useState(0);
   const [blue, setBlue] = useState(0);
 
-  const COLOR_COUNTER = 15;
+  const setColor = (color, change) => {
+    switch (color) {
+      case "red":
+        red + change > 255 || red + change < 0 ? null : setRed(red + change);
+        break;
+
+      case "green":
+        green + change > 255 || green + change < 0
+          ? null
+          : setGreen(green + change);
+        break;
+      case "blue":
+        blue + change > 255 || blue + change < 0
+          ? null
+          : setBlue(blue + change);
+      default:
+        break;
+    }
+  };
 
   return (
     <View>
       <ColorCounter
-        onIncrease={() => setRed(red + COLOR_COUNTER)}
-        onDecrease={() => setRed(red - COLOR_COUNTER)}
+        onIncrease={() => setColor("red", +COLOR_COUNTER)}
+        onDecrease={() => setColor("red", -1 * COLOR_COUNTER)}
         color="Red"
       />
       <ColorCounter
-        onIncrease={() => setGreen(green + COLOR_COUNTER)}
-        onDecrease={() => setGreen(green - COLOR_COUNTER)}
+        onIncrease={() => setColor("green", +COLOR_COUNTER)}
+        onDecrease={() => setColor("green", -1 * COLOR_COUNTER)}
         color="Green"
       />
       <ColorCounter
-        onIncrease={() => setBlue(blue + COLOR_COUNTER)}
-        onDecrease={() => setBlue(blue - COLOR_COUNTER)}
+        onIncrease={() => setColor("blue" + COLOR_COUNTER)}
+        onDecrease={() => setColor("blue" - 1 * COLOR_COUNTER)}
         color="Blue"
       />
       <View
